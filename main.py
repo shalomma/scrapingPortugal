5920b723fe31219bb6f96e032c3c86d98a150168
Maayan Shalom <sha.maayan@gmail.com>
2021-08-11 01:17:07 +0300

    anticaptchaofficial

--- main.py
+++ main.py
@@ -10,6 +10,26 @@ from webdriver_manager.chrome import ChromeDriverManager
 from ibm_watson import SpeechToTextV1
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 from selenium.webdriver.common.action_chains import ActionChains
+from anticaptchaofficial.imagecaptcha import imagecaptcha
+from PIL import Image
+
+
+def solve_img_captcha():
+    solver = imagecaptcha()
+    solver.set_verbose(1)
+    solver.set_key(os.environ['anti_key'])
+
+    im = Image.open(img_file)
+    im1 = im.crop((700, 500, 1700, 1000))
+    im1.save(img_file)
+
+    captcha_text = solver.solve_and_return_solution(img_file)
+    if captcha_text != 0:
+        print("captcha text " + captcha_text)
+    else:
+        print("task finished with error " + solver.error_code)
+    driver.find_element(By.CSS_SELECTOR, r'#grantSchedulingFormID\:captchaCode').send_keys(captcha_text), time.sleep(2)
+    driver.find_element(By.CSS_SELECTOR, r'#grantSchedulingFormID\:grantSchedulingContinueBtnID').click()
 
 
 def STT():
@@ -36,7 +56,7 @@ def down_submit(a_link, p_elem):
     for file in os.listdir():
         if file.endswith('.wav') or file.endswith('.png'):
             os.remove(file)
-    driver.execute_script(f"window.open('{a_link}');"), time.sleep(3)
+    # driver.execute_script(f"window.open('{a_link}');"), time.sleep(3)
     actionChains = ActionChains(driver)
     actionChains.move_to_element(p_elem).context_click().perform()
     for c in 'search':
@@ -46,15 +66,15 @@ def down_submit(a_link, p_elem):
     driver.switch_to.window(driver.window_handles[1])
     img_link = driver.find_element_by_css_selector('.card-section img').get_attribute('src')
     driver.get(img_link)
-    driver.get_screenshot_as_file('./captcha.png')
+    driver.get_screenshot_as_file(img_file)
     driver.switch_to.window(driver.window_handles[0])
     close_free_tabs()
-    text = STT()
-    if not text:
-        text = 'None'
-    print('text is ', text)
-    driver.find_element(By.CSS_SELECTOR, '#grantSchedulingFormID\:captchaCode').send_keys(text), time.sleep(2)
-    driver.find_element(By.CSS_SELECTOR, '#grantSchedulingFormID\:grantSchedulingContinueBtnID').click()
+    # text = STT()
+    # if not text:
+    #     text = 'None'
+    # print('text is ', text)
+    # driver.find_element(By.CSS_SELECTOR, '#grantSchedulingFormID\:captchaCode').send_keys(text), time.sleep(2)
+    # driver.find_element(By.CSS_SELECTOR, '#grantSchedulingFormID\:grantSchedulingContinueBtnID').click()
 
 
 def solution():
@@ -75,7 +95,7 @@ def validate():
         driver.find_element(By.CSS_SELECTOR, 'img.BDC_ReloadIcon').click(), time.sleep(2)
         audio_link = driver.find_element(By.CSS_SELECTOR, '#exampleCaptcha_SoundLink').get_attribute('href')
         img_elem = driver.find_element(By.XPATH, '//img[contains(@id,"CaptchaImage")]')
-        captcha_elem = driver.find_element(By.CSS_SELECTOR, '#grantSchedulingFormID\:captchaCode')
+        captcha_elem = driver.find_element(By.CSS_SELECTOR, r"#grantSchedulingFormID\:captchaCode")
         captcha_elem.send_keys(Keys.CONTROL, 'a')
         captcha_elem.send_keys(Keys.DELETE)
         down_submit(audio_link, img_elem)
@@ -85,11 +105,6 @@ def validate():
             validate()
     except:
         pass
-
-
-def lanch_browser(url):
-    driver.get(url)
-    driver.maximize_window()
 
 
 def close_free_tabs():
@@ -114,6 +129,7 @@ if __name__ == '__main__':
     options.add_argument('start-maximized')
 
     url = 'https://agendamentosonline.mne.pt/AgendamentosOnline/index.jsf'
+    img_file = 'captcha.png'
     driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
     driver.implicitly_wait(10)
     wait = WebDriverWait(driver, 15)
@@ -127,20 +143,24 @@ if __name__ == '__main__':
     # Nº b. i
     driver.find_element(By.XPATH, '//input[@id = "scheduleForm:tabViewId:ccnum"]').send_keys(9058392233)
     # Data de nascimento
-    driver.find_element(By.XPATH, '//input[@id = "scheduleForm:tabViewId:dataNascimento_input"]').send_keys('24-04-1987')
+    driver.find_element(By.XPATH,
+                        '//input[@id = "scheduleForm:tabViewId:dataNascimento_input"]').send_keys('24-04-1987')
     # Pesquisar
     driver.find_element(By.XPATH, '//span[contains(text(),"Pesquisar")]').click()
     # Posto
-    Post_elem = driver.find_element(By.XPATH,
-                                    '//div[@id="scheduleForm:postcons_panel"]//ul[contains(@class,"ui-widget-content")]/li[2]')
+    Post_elem = \
+        driver.find_element(By.XPATH,
+                            '//div[@id="scheduleForm:postcons_panel"]//ul[contains(@class,"ui-widget-content")]/li[2]')
     driver.execute_script("arguments[0].click();", Post_elem)
     # Categoria do Ato Consular
-    Cat_elem = driver.find_element(By.XPATH,
-                                   '//div[@id="scheduleForm:categato_panel"]//ul[contains(@class,"ui-widget-content")]/li[3]')
+    Cat_elem = \
+        driver.find_element(By.XPATH,
+                            '//div[@id="scheduleForm:categato_panel"]//ul[contains(@class,"ui-widget-content")]/li[3]')
     driver.execute_script("arguments[0].click();", Cat_elem)
     # Ato Consular
-    Consular_elem = driver.find_element(By.XPATH,
-                                        '//div[@id="scheduleForm:atocons_panel"]//ul[contains(@class,"ui-widget-content")]/li[2]')
+    Consular_elem = \
+        driver.find_element(By.XPATH,
+                            '//div[@id="scheduleForm:atocons_panel"]//ul[contains(@class,"ui-widget-content")]/li[2]')
     driver.execute_script("arguments[0].click();", Consular_elem)
     # Adicionar Ato Consular
     driver.find_element(By.XPATH, '//span[contains(text(),"Ato Consular")]').click()
@@ -152,7 +172,8 @@ if __name__ == '__main__':
         pass
     # Calendarizar
     driver.find_element(By.XPATH, '//span[contains(text(),"Calendarizar")]').click()
-    # Captcha Solutilon
+    # Captcha Solution
     wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '[for="grantSchedulingFormID:captchaCode"]')))
     solution()
-    validate()
+    solve_img_captcha()
+    # validate()
